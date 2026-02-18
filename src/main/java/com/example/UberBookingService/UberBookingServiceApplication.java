eee8720699f2267d5ca668c370b1d443f513635e
sahilzambre05 <sahildinesh.zambre2023@vitstudent.ac.in>
2026-02-18 14:39:47 +0530

    first commit

---
+++ src/main/java/com/example/UberBookingService/UberBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.UberBookingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberBookingServiceApplication.class, args);
+	}
+
+}
